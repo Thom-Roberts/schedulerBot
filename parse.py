053a2f64dbdd6f29c983c941549358b9ae41014c
Thomas Roberts <robertsorange@gmail.com>
2018-06-24 18:52:30 -0600

    How to set up an XML file

---
+++ parse.py
@@ -0,0 +1,28 @@
+#I want to write and read to an xml file
+#I'm writing to bot.xml
+#If the second argument is write, you write to the file. If its read, read from it
+import sys
+import xml.etree.cElementTree as ET
+
+#how you get arguments
+"""print ("This is the name of the script: ", sys.argv[0])
+print ("Number of arguments: ", len(sys.argv))
+print ("The arguments are: " , str(sys.argv))"""
+
+action = sys.argv[1]
+tree = ET.parse('bot.xml')
+root = tree.getroot()
+if action == 'write':
+    #if there is something to write
+    print (len(sys.argv))
+    if len(sys.argv) == 3:
+        ET.SubElement(root, "item").text = sys.argv[2]
+        tree = ET.ElementTree(root)
+        tree.write('bot.xml')
+    else:
+        print("Please provide a value to write")
+elif action == 'read':
+    for item in root:
+        print(item.text)
+else:
+    print("Command not recognized")
